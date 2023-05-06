0a67e21589b088dfedccc9f57a4a2ab44b60af6b
Ricardo de Monteiro e Tavares <rmonteiro@marlin.com.br>
2023-05-06 18:23:48 -0300

    feat: adição do dicionário de rotas

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,10 @@
 import type { V2_MetaFunction } from "@remix-run/node";
 
+export enum Location {
+  home = '/',
+  jobs = '/vagas',
+};
+
 export const meta: V2_MetaFunction = () => {
   return [{ title: "New Remix App" }];
 };

f45ea402e428fb85783091dc3b83cdeb5684ac1c
Ricardo de Monteiro e Tavares <rmonteiro@marlin.com.br>
2023-05-07 21:38:24 -0300

    feat: adição do tailwind css

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,7 +10,10 @@ export const meta: V2_MetaFunction = () => {
 };
 
 export default function Index() {
-  return (
+    return (<h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>);
+  /* return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
       <ul>
@@ -39,5 +42,5 @@ export default function Index() {
         </li>
       </ul>
     </div>
-  );
+  ); */
 }
